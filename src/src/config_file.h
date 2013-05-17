953b8ea82cbe5119a9e767b777982dbd90daae15
Piotr <piotr@piotr.com>
2013-05-17 15:47:05 +0200

    removed junk

--- src/src/config_file.h
+++
@@ -1,6 +0,0 @@
-
-
-
-
-int process_config_file(struct signature **arr_lines2,int* signatures,int num_lines, char* config_file);
-char *get_substring_value(char* str);
--- src/src/log.h
+++
@@ -1,8 +0,0 @@
-#define MAX_LOG_MSG_LEN 200
-#include "Configuration.h"
-
-class Configuration;
-extern Configuration* configuration;
-
-void log_create(const char* file);
-void log_write(const char* msg);
